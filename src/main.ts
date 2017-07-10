a714789915aacd74137f8c385b6f15ad3e907ccb
wsyski <wos@axiell.com>
2017-07-10 15:12:43 +0200

    Renoved zone.js

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import {createAppTodoModule} from "./app-todo/app-todo.module";
 import {createAppHeroesModule} from "./app-heroes/app-heroes.module";
 
-export function RunAppTodo(portletNamespace: string) {
+export function RunApp_todo(portletNamespace: string) {
     platformBrowserDynamic()
         .bootstrapModule(createAppTodoModule(portletNamespace))
         .then(success => console.log(`Bootstrap Todo success`))
         .catch(err => console.error(err));
 }
 
-export function RunAppHeroes(portletNamespace: string) {
+export function RunApp_heroes(portletNamespace: string) {
     platformBrowserDynamic()
         .bootstrapModule(createAppHeroesModule(portletNamespace))
         .then(success => console.log(`Bootstrap Todo success`))
