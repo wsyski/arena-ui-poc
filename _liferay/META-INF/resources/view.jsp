27eb80df190de3d2226bbcf964794b787a0713c0
wsyski <wos@axiell.com>
2017-07-12 16:39:18 +0200

    Removed uirouter

--- _liferay/META-INF/resources/view.jsp
+++ _liferay/META-INF/resources/view.jsp
@@ -1,9 +1,9 @@
 <%@ include file="/init.jsp" %>
 
-<app-<%= portletDisplay.getPortletName().replace('_','-') %> id="<portlet:namespace/>">Loading...</app-<%= portletDisplay.getPortletName().replace('_','-') %>>
+<app-<%= portletDisplay.getPortletName().replaceAll("[_\\.]","-") %> id="<portlet:namespace/>">Loading...</app-<%= portletDisplay.getPortletName().replaceAll("[_\\.]","-") %>>
 
 <script type="text/javascript">
-  AxUtil.scriptLoader.loadAll(["/o/@@bundlePath/shim.js", "/o/@@bundlePath/zone.js", "/o/@@bundlePath/ng-runtime-dll.js", "/o/@@bundlePath/main.js"],function () {
-    Main.RunApp_<%= portletDisplay.getPortletName().replace('.','_') %>("<portlet:namespace/>");
+  AxUtil.scriptLoader.loadAll(["/o/@@bundlePath/shim.js", "/o/@@bundlePath/zone.js",  "/o/@@bundlePath/ng-runtime-dll.js", "/o/@@bundlePath/main.js"], function () {
+    window["AxMain_" + "@@bundleSymbolicName".replace(/[-\.]/g, '_')].RunApp_<%= portletDisplay.getPortletName().replaceAll("[-\\.]","_") %>("<portlet:namespace/>");
   });
 </script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,44 @@
-var webpack = require('webpack');
-var path = require('path');
+let webpack = require('webpack');
+let path = require('path');
 
 const appRoot = path.resolve('./src');
+
+let bundle_options = require('./bundle.json');
+let bundle_symbolic_name = bundle_options['options'].bundleSymbolicName;
+let webpack_entry = `AxMain_${bundle_symbolic_name.replace(/[-\.]/g, '_')}`;
 
 module.exports = {
   bail: true,
   entry: {
     // NOTE: entry settings will be overridden when invoked from gulp
-    'main'  : ['./src/main.ts']
+    'main': ['./src/main.ts']
   },
   output: {
-    filename: '[name].js',
-    // NOTE: path will be overridden when invoked from gulp
-    path : path.resolve('./build'),
+    filename: '[name].js', // NOTE: path will be overridden when invoked from gulp
+    path: path.resolve('./build'),
     devtoolModuleFilenameTemplate: '[resource-path]',
     libraryTarget: 'var',
-    library: 'Main'
+    library: webpack_entry
   },
-  plugins: [
-    new webpack.DllReferencePlugin({
-      context: '.',
-      manifest: require(path.join(__dirname,'ng-runtime','ng-runtime-manifest.json'))
-    }),
-    // suppress Typescript warnings when building Angular into vendor package
+  plugins: [new webpack.DllReferencePlugin({
+    context: '.',
+    manifest: require(path.join(__dirname, 'ng-runtime', 'ng-runtime-manifest.json'))
+  }), // suppress Typescript warnings when building Angular into vendor package
     //new webpack.ContextReplacementPlugin(/angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,appRoot),
-    new webpack.ContextReplacementPlugin(/angular(\\|\/)core(\\|\/)@angular/,appRoot),
- ],
+    new webpack.ContextReplacementPlugin(/angular(\\|\/)core(\\|\/)@angular/, appRoot),],
   resolve: {
-    extensions: ['*','.ts', '.js']
-  }, 
+    extensions: ['*', '.ts', '.js']
+  },
   module: {
-    loaders: [
-      // process Angular templates to inline HTML/CSS then invoke Typescript
-      {test: /\.ts$/, loader: 'ts-loader!angular2-template-loader'},
-      // used to load Angular HTML/CSS files for templates
-      {test: /\.(html|css)$/, loader: 'raw-loader' },
-    ],
+    loaders: [// process Angular templates to inline HTML/CSS then invoke Typescript
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader!angular2-template-loader'
+      }, // used to load Angular HTML/CSS files for templates
+      {
+        test: /\.(html|css)$/,
+        loader: 'raw-loader'
+      },],
     noParse: /path.join(__dirname,'node_modules', 'angular2', 'bundles')/
   },
   devtool: 'source-map'
