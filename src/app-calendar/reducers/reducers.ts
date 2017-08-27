564c20d2f8f30c78b39607c560e50a1c949e92af
Wojciech Syski <wsyski@gmail.com>
2017-08-27 15:00:52 +0200

    Added portlet configuration and translation

--- src/app-calendar/reducers/reducers.ts
+++ src/app-calendar/reducers/reducers.ts
@@ -1,21 +1,23 @@
 import * as fromSearch from './search-reducer';
+import {routerReducer} from '@ngrx/router-store';
 
 export interface State {
-  search: fromSearch.State;
+    search: fromSearch.State;
 }
 
 export const reducers = {
-  search: fromSearch.reducer
+    search: fromSearch.reducer,
+    router: routerReducer
 };
 
 export function selectResults(state: State) {
-  return state.search.results;
+    return state.search.results;
 }
 
 export function selectCount(state: State) {
-  return state.search.results.length;
+    return state.search.results.length;
 }
 
 export function selectTerms(state: State) {
-  return state.search.searchTerms;
+    return state.search.searchTerms;
 }
